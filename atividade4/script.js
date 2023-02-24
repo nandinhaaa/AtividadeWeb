c7d3a42e29270bfdc56336f137a80530eb0fa3d9
Maria Fernanda <91507393+nandinhaaa@users.noreply.github.com>
2023-02-24 19:05:51 -0300

    Update script.js

--- atividade4/script.js
+++ atividade4/script.js
@@ -55,7 +55,7 @@ function finalizar() {
         res.innerHTML += `<p> Ao todo, temos ${tot} numero cadastrados </p>`
         res.innerHTML += `<p> O maior valor informado foi ${maior}.</p>`
         res.innerHTML += `<p> O menor valor informado foi ${menor}.</p>`
-        res.innerHTML += `<p> Sonmando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p> Somando todos os valores, temos ${soma}.</p>`
         res.innerHTML += `<p> A média dos valores digitados é: ${media}.</p>`
 
     }
